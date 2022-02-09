d0d556a616513223da48748806730c7f2208677a
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-02-09 15:43:52 -0300

    Requisito 2

---
+++ src/pages/Search.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</div>
+    )
+  }
+}
