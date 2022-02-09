0bad2ab5997e649990c85361fc7a65cefb6c0c9b
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-02-09 17:52:54 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { Route, Switch } from 'react-router-dom';
-import Search from './pages/Search';
-import * as api from './services/api';
+import QuerySearch from './pages/QuerySearch';
+/* import * as api from './services/api'; */
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ Search } />
+        <Route path="/" component={ QuerySearch } />
       </Switch>
     </BrowserRouter>
   );
